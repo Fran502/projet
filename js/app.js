e07b91abec3012e64894ce0148c1a89092abf550
Anna <anna.sollazzo@shaw.ca>
2019-01-22 10:23:07 -0800

    path fixes!

--- js/app.js
+++ js/app.js
@@ -6,25 +6,25 @@ app.config(function($routeProvider) {
         templateUrl : "/projet/views/acceuil.html"
     })
     .when("/Catherine", {
-        templateUrl : "../views/catherine.html"
+        templateUrl : "/projet/views/catherine.html"
     })
     .when("/Rachel", {
-        templateUrl : "../views/rachel.html"
+        templateUrl : "/projet/views/rachel.html"
     })
     .when("/Sandra", {
-        templateUrl : "../views/sandra.html"
+        templateUrl : "/projet/views/sandra.html"
     })
     .when("/Ryleigh", {
-        templateUrl : "../views/ryleigh.html"
+        templateUrl : "/projet/views/ryleigh.html"
     })
     .when("/Pamela", {
-        templateUrl : "../views/pamela.html"
+        templateUrl : "/projet/views/pamela.html"
     })
     .when("/Francesca", {
-        templateUrl : "../views/francesca.html"
+        templateUrl : "/projet/views/francesca.html"
     })
     .when("/Anna", {
-        templateUrl : "../views/anna.html"
+        templateUrl : "/projet/views/anna.html"
     })
 
 });
