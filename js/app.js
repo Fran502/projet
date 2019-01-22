bbefde0dc093064354eb45ec1685cc693bba7cda
Anna <anna.sollazzo@shaw.ca>
2019-01-22 10:14:07 -0800

    another try at paths

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('fran502', ['ngAnimate', 'ngRoute', 'ui.bootstrap']);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "../acceuil.html"
+        templateUrl : "/project/acceuil.html"
     })
     .when("/Catherine", {
         templateUrl : "../views/catherine.html"
